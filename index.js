ca578d08169d0dc625d437f37b6083f1013361d1
unel <enee.prog@gmail.com>
2025-03-21 16:51:36 +0100

    add current data

--- index.js
+++ index.js
@@ -20,13 +20,18 @@ function search() {
 			document.getElementById(
 				'map'
 			).src = `https://www.openstreetmap.org/export/embed.html?bbox=${boundingBox[2]},${boundingBox[0]},${boundingBox[3]},${boundingBox[1]}&layer=mapnik`;
-			updatefutreWeather(data[0].lon, data[0].lat);
+
+			console.log(data);
+			document.querySelector('.information > div:nth-child(2) > div:nth-child(1)').textContent =
+				data[0].display_name.split(',')[0];
+
+			calculateWeather(data[0].lon, data[0].lat);
 		});
 }
 
-function updatefutreWeather(lon, lat) {
+function calculateWeather(lon, lat) {
 	fetch(
-		`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,weather_code&current=temperature_2m&timezone=Europe%2FLondon&forecast_days=2`
+		`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,weather_code&current=temperature_2m,wind_speed_10m,relative_humidity_2m&timezone=Europe%2FLondon&forecast_days=2`
 	)
 		.then(response => response.json())
 		.then(data => {
@@ -34,34 +39,61 @@ function updatefutreWeather(lon, lat) {
 				alert('Ville non trouvée.');
 				return;
 			}
-			console.log(data.hourly, data.hourly.time.length, data.hourly.temperature_2m.length);
-			const offsetTime = new Date().getHours();
-			if (
-				!data.hourly ||
-				!data.hourly.time ||
-				!data.hourly.temperature_2m ||
-				!data.hourly.weather_code ||
-				/* dataset have enough data */
-				data.hourly.time.length < offsetTime + 10 ||
-				data.hourly.temperature_2m.length < offsetTime + 10 ||
-				data.hourly.weather_code.length < offsetTime + 10
-			) {
-				alert('données manquante');
-				return;
-			}
-
-			//change for data for futre weather
-			for (let i = 1; i <= 10; i++) {
-				const urlImg = weatherIcon + weatherCodeToIcon(data.hourly.weather_code[i + offsetTime]);
-				document.querySelector(`.futreWeather > div:nth-child(${i}) > div:nth-child(1)`).textContent =
-					new Date(data.hourly.time[i + offsetTime]).getHours() + ' h';
-				const img = document.querySelector(`.futreWeather > div:nth-child(${i}) > img`);
-				img.setAttribute('alt', 'weather code ' + data.hourly.weather_code[i + offsetTime]);
-				img.setAttribute('src', urlImg);
-				document.querySelector(`.futreWeather > div:nth-child(${i}) > div:nth-child(3)`).textContent =
-					data.hourly.temperature_2m[i + offsetTime] + '°';
-			}
+			futureWeather(data);
+			currentWeather(data);
 		});
+}
+
+function futureWeather(data) {
+	const offsetTime = new Date().getHours();
+	if (
+		!data.hourly ||
+		!data.hourly.time ||
+		!data.hourly.temperature_2m ||
+		!data.hourly.weather_code ||
+		/* dataset have enough data */
+		data.hourly.time.length < offsetTime + 10 ||
+		data.hourly.temperature_2m.length < offsetTime + 10 ||
+		data.hourly.weather_code.length < offsetTime + 10
+	) {
+		alert('données manquante');
+		return;
+	}
+
+	//change for data for futre weather
+	for (let i = 1; i <= 10; i++) {
+		const urlImg = weatherIcon + weatherCodeToIcon(data.hourly.weather_code[i + offsetTime]);
+		document.querySelector(`.futreWeather > div:nth-child(${i}) > div:nth-child(1)`).textContent =
+			new Date(data.hourly.time[i + offsetTime]).getHours() + ' h';
+		const img = document.querySelector(`.futreWeather > div:nth-child(${i}) > img`);
+		img.setAttribute('alt', 'weather code ' + data.hourly.weather_code[i + offsetTime]);
+		img.setAttribute('src', urlImg);
+		document.querySelector(`.futreWeather > div:nth-child(${i}) > div:nth-child(3)`).textContent =
+			data.hourly.temperature_2m[i + offsetTime] + '°';
+	}
+}
+
+function currentWeather(data) {
+	if (
+		!data.current ||
+		typeof data.current.relative_humidity_2m != 'number' ||
+		typeof data.current.temperature_2m != 'number' ||
+		typeof data.current.wind_speed_10m != 'number'
+	) {
+		alert('données manquante');
+		return;
+	}
+	const urlImg = weatherIcon + weatherCodeToIcon(data.hourly.weather_code);
+
+	const img = document.querySelector('.information > img');
+	img.setAttribute('alt', 'weather code ' + data.current.weather_code);
+	img.setAttribute('src', urlImg);
+	document.querySelector('.information > div:nth-child(3) > div:nth-child(1)').textContent =
+		data.current.temperature_2m + '°';
+	document.querySelector('.information > div:nth-child(4) > div:nth-child(1)').textContent =
+		data.current.relative_humidity_2m + '%';
+	document.querySelector('.information > div:nth-child(5) > div:nth-child(1)').textContent =
+		data.current.wind_speed_10m + 'km/h';
 }
 
 function chartFakeData() {
